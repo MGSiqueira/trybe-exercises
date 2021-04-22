a22be5ddc520af81a4b6f6e698bb8d7d56751ce9
MGSiqueira <marcos.siqueira98@gmail.com>
2021-04-22 17:27:59 -0300

    Resolve o exercício 3

---
+++ bloco_04/dia_1/ex_03/maior_de_tres.js
@@ -0,0 +1,16 @@
+let a = 8;
+let b = 7;
+let c = 8;
+
+if (a > c && a > b) {
+    console.log('a o maior')
+}
+else if (b > a && b > c) {
+    console.log('b é o maior')
+}
+else if (c > a && c > b) {
+    console.log('c é o maior')
+}
+else {
+    console.log('não existe UM maior')
+}
