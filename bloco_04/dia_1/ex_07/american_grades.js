2a99b7fc720200dcc8589a22e5e56388ae97096f
MGSiqueira <marcos.siqueira98@gmail.com>
2021-04-22 18:23:06 -0300

    Resolve o exercício 7

---
+++ bloco_04/dia_1/ex_07/american_grades.js
@@ -0,0 +1,20 @@
+let grade = 100;
+let validGrade = grade >= 0 && grade <= 100;
+
+if (validGrade) {
+    if (grade >= 90) {
+        console.log('A');
+    } else if (grade >= 80) {
+        console.log('B');
+    } else if (grade >= 70) {
+        console.log('C');
+    } else if (grade >= 60) {
+        console.log('D');
+    } else if (grade >= 50) {
+        console.log('E');
+    } else {
+        console.log('F');
+    } 
+} else {
+    console.log('Invalid grade.');
+}
