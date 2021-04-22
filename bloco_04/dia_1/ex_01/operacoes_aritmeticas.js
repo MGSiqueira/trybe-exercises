497fc57d05f50858e3db2a6221979cf4dfe2e782
MGSiqueira <marcos.siqueira98@gmail.com>
2021-04-22 16:46:29 -0300

    Cria os diretórios bloco_04 > dia_1 > ex_01 e resolve o exercício 1

---
+++ bloco_04/dia_1/ex_01/operacoes_aritmeticas.js
@@ -0,0 +1,15 @@
+let a = 4;
+let b = 2;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let resto = a % b;
+
+console.log('Adição\n', adicao);
+console.log('Subtação\n', subtracao);
+console.log('Multiplicação\n', multiplicacao);
+console.log('Divisão\n', divisao);
+console.log('Resto da divisão\n', resto);
+
+
