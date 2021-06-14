8437477cf66632de33736bb9b39230c8dd5efb0c
MGSiqueira <marcos.siqueira98@gmail.com>
2021-06-14 14:56:20 -0300

    Bloco 10

---
+++ bloco_10/dia_1/ex_1/ex_1.test.js
@@ -0,0 +1,9 @@
+const sum = require('./ex_1');
+
+test('Testa se o retorno de sum(4, 5) é igual a 9.', () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test('Testa se o retorno de sum(0, 0) é igual a 0.', () => {
+  expect(sum(0, 0)).toBe(0);
+});
