33927f2ced7b918f2bba06c7ba8323764196f5e6
MGSiqueira <marcos.siqueira98@gmail.com>
2021-06-21 20:16:26 -0300

    Exercicios dia 1 bloco 11

---
+++ bloco_11/dia_1/exemplo-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import component from './Component.js'
+
+function App() {
+  return component();
+}
+
+export default App;
---
+++ bloco_11/dia_1/exemplo-app/src/Component.js
@@ -0,0 +1,8 @@
+const name = <h1>Marcos Siqueira</h1>
+const paragraph = <p>I am nobody, but you can call me everybody.</p>
+
+function component() {
+  return <div>{name} {paragraph}</div>
+}
+
+export default component;
